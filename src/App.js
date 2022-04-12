65d0dfefab1a787cd711f8f96ac819a622641d79
GeeeShep <gashep@yahoo.com>
2022-04-12 15:44:14 -0700

    created 3 by 3 grid

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react'
 import Square from './components/Square'
 import './App.css'
+import './components/Square.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }
+      squares: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+          }
   }
 
+
+  
   render(){
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div className="gameBoard">
+              <Square id="sq1"/>
+              <Square id="sq2"/>
+              <Square id="sq3"/>
+              <Square id="sq4"/>
+              <Square id="sq5"/>
+              <Square id="sq6"/>
+              <Square id="sq7"/>
+              <Square id="sq8"/>
+              <Square id="sq9"/>
+              
+        </div>
       </>
     )
   }
